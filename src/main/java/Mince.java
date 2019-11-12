49729640bcef23565e7fda4676ae85ebae305a29
tomaspribyl123 <1tomaspribyl@seznam.cz>
2019-11-12 15:20:47 +0100

    Update Mince.java
    #1 commit

--- src/main/java/Mince.java
+++ src/main/java/Mince.java
@@ -15,16 +15,4 @@ public class Mince {
     public void setHodnotaMince(int hodnotaMince) {
         this.hodnotaMince = hodnotaMince;
     }
-
-    private Map<Integer, Mince> MinceMap = new HashMap<>();
-
-    private void Mince(){
-        MinceMap.put(1, new Mince(1));
-        MinceMap.put(2, new Mince(2));
-        MinceMap.put(3, new Mince(5));
-        MinceMap.put(4, new Mince(10));
-        MinceMap.put(5, new Mince(20));
-        MinceMap.put(6, new Mince(50));
-
-    }
 }
